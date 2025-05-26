c5e7afddeec700597cb969f936a273e58cb4825d
AshishLakhimale23 <ashishlakhimale23@gmail.com>
2025-05-26 17:32:45 +0530

    converted usersConnected variable to in memory varibale using redis (it was a in memory variable but shifted it to redis to avoid the loss if the server restarts )

--- apps/websocket-backend/src/RoomManager.ts
+++ apps/websocket-backend/src/RoomManager.ts
@@ -1,7 +1,7 @@
 import { WebSocket } from "ws"
 import { prisma } from "@repo/db/prisma"
 import { singleton } from "./singleton"
-import {createClient} from "redis"
+import { createClient } from "redis"
 
 interface Users {
     socket: WebSocket,
@@ -9,28 +9,28 @@ interface Users {
 }
 
 export class RoomManager {
-    
-    private redisPublisher 
+
+    private redisPublisher
     private redisSubscriber
     private redisClient
     // this should be a in memory varible also adding a snapshot to it  (redis in memory variable)
-    private subscribedChannels : string = "subscribedChannels" 
-    // should i also make this a in memory variable
-    private usersConnected: Users[]
+    private subscribedChannels: string = "subscribedChannels"
+    // i get it i get it mann the socket come different when we connect and reconnect so the resisted data in the redis check for the socket and it says it dosent exist so but the use exists so now just store the userId
+    // should i also make this a in memory variable also make it a set too 
+    private usersConnected: string = "usersConnected"
 
     constructor() {
 
-        this.usersConnected = []
-        this.redisClient = createClient({url:"redis://localhost:6379"})
+        this.redisClient = createClient({ url: "redis://localhost:6379" })
         this.redisPublisher = createClient({ url: "redis://localhost:6379" });
         this.redisSubscriber = createClient({ url: "redis://localhost:6379" });
         this.connectRedisClients()
-        
+
     }
 
     private async connectRedisClients() {
         try {
-            if(!this.redisClient.isOpen){
+            if (!this.redisClient.isOpen) {
                 await this.redisClient.connect()
             }
             if (!this.redisPublisher.isOpen) {
@@ -39,7 +39,7 @@ export class RoomManager {
             if (!this.redisSubscriber.isOpen) {
                 await this.redisSubscriber.connect();
                 //resubscribing to all the channels in are reconnecting 
-                this.redisSubscriber.on("reconnecting",async ()=>{
+                this.redisSubscriber.on("reconnecting", async () => {
                     if (await this.redisClient.sCard(this.subscribedChannels) !== 0) {
                         for (const values of this.subscribedChannels) {
                             await this.redisSubscriber.subscribe(
@@ -51,32 +51,32 @@ export class RoomManager {
                         }
                     }
                 })
-                 
+
             }
         } catch (error) {
             console.error("Error connecting to Redis:", error);
         }
     }
 
-    handleIncomingMessage(channel:string,message:string){
-        try{
+    handleIncomingMessage(channel: string, message: string) {
+        try {
             const parsedMessage = JSON.parse(message)
-            singleton.bordcast(parsedMessage.message,parsedMessage.roomId,parsedMessage.userId)
-        }catch(error){
+            singleton.bordcast(parsedMessage.message, parsedMessage.roomId, parsedMessage.userId)
+        } catch (error) {
             console.error("Error handling incoming message:", error);
         }
 
     }
 
-    addUser(user: Users) {
+    async addUser(user: Users) {
 
-        const userConnected = this.usersConnected.find(previousUser => previousUser.userId == user.userId)
-        if (userConnected) {
+        const userConnected = await this.redisClient.sIsMember(this.usersConnected, JSON.stringify(user.userId))
+        if (userConnected == 1) {
             console.log("the user is already connected ...")
             return
         }
 
-        this.usersConnected.push(user)
+        await this.redisClient.sAdd(this.usersConnected, JSON.stringify(user.userId))
         this.addHandler(user)
     }
 
@@ -87,25 +87,26 @@ export class RoomManager {
                 console.log("Received:", parsedData);
                 switch (parsedData.type) {
                     case "join_room":
-                        const userExists = this.usersConnected.find((x) => x.userId === user.userId);
-                        if (userExists) {
+                        const userExists = await this.redisClient.sIsMember(this.usersConnected, JSON.stringify(user.userId));
+                        console.log("userExists",userExists)
+                        if (userExists == 1) {
                             console.log("reached here....")
-                            if(!await this.redisClient.sIsMember(this.subscribedChannels,parsedData.roomId.toString())){
+                            if (await this.redisClient.sIsMember(this.subscribedChannels, parsedData.roomId.toString()) == 0) {
                                 await this.redisSubscriber.subscribe(
                                     parsedData.roomId.toString(),
                                     (message: string, channel: string) => {
                                         this.handleIncomingMessage(channel, message);
                                     }
                                 );
-                                await this.redisClient.sAdd(this.subscribedChannels,parsedData.roomId)
+                                await this.redisClient.sAdd(this.subscribedChannels, parsedData.roomId)
                             }
                             singleton.addUser(user, parsedData.roomId.toString());
                             console.log(`User ${user.userId} joined room ${parsedData.roomId}`);
                         }
                         break;
                     case "message":
-                        const userExistsForMessage = this.usersConnected.find((x) => x.userId === user.userId);
-                        if (userExistsForMessage) {
+                        const userExistsForMessage = await this.redisClient.sIsMember(this.usersConnected, JSON.stringify(user.userId));
+                        if (userExistsForMessage == 1) {
                             try {
                                 const dataTosend = {
                                     ...parsedData,
@@ -118,7 +119,7 @@ export class RoomManager {
                                         userId: user.userId
                                     }
                                 })
-                                await this.redisPublisher.publish(parsedData.roomId,JSON.stringify(dataTosend))
+                                await this.redisPublisher.publish(parsedData.roomId, JSON.stringify(dataTosend))
                                 console.log(`User ${user.userId} messaged ${parsedData.roomId}`);
                             } catch (error) {
                                 console.log(error)
@@ -133,10 +134,10 @@ export class RoomManager {
     }
 
 
-    removeUser(user: Users) {
-        const index = this.usersConnected.findIndex((users) => users.socket === user.socket);
+    async removeUser(user: Users) {
+        const index = await this.redisClient.sIsMember(this.usersConnected, JSON.stringify(user.userId));
 
-        if (index === -1) {
+        if (index == 1) {
             console.log("user doesn't exist");
             return;
         }
@@ -144,18 +145,18 @@ export class RoomManager {
         // get the rooms with zero users of the users which is disconnecting and unsubscribe it .
         const roomToUnsubscribe = singleton.getRoomWithZeroUsers(user.userId)
 
-        if(roomToUnsubscribe.length>0){
-            roomToUnsubscribe.forEach(async (roomId)=>{
-                if(await this.redisClient.sIsMember(this.subscribedChannels,roomId)){
-                    this.redisSubscriber.unsubscribe(roomId,()=>{
+        if (roomToUnsubscribe.length > 0) {
+            roomToUnsubscribe.forEach(async (roomId) => {
+                if (await this.redisClient.sIsMember(this.subscribedChannels, roomId) == 1) {
+                    this.redisSubscriber.unsubscribe(roomId, () => {
                         console.log(`unsubscribed ${roomId}`)
                     })
-                    await this.redisClient.sRem(this.subscribedChannels,roomId)
+                    await this.redisClient.sRem(this.subscribedChannels, roomId)
                 }
             })
-        } 
+        }
 
-        this.usersConnected.splice(index, 1);
+        await this.redisClient.sRem(this.usersConnected, JSON.stringify(user.userId));
         singleton.removeUser(user);
     }
 
